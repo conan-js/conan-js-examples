9366e26f38a158f3426aa6ec4faa963aad8b7d36
almansa@conan-ui.com <almansa@conan-ui.com>
2020-04-08 17:01:42 +0200

    Uploading changes from source package.

--- todo-list/src/renderers/addTodo.renderer.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { ToDo } from "../domain/domain";
-import { IConsumer } from "conan-ui-core";
-interface AddTodoProps {
-    onClick: IConsumer<ToDo>;
-}
-export declare const AddTodo: (props: AddTodoProps) => JSX.Element;
-export {};
--- todo-list/src/renderers/todo.renderer.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { ICallback } from "conan-ui-core";
-interface TodoProps {
-    onClick: ICallback;
-    completed: boolean;
-    text: string;
-}
-export declare const Todo: (props: TodoProps) => JSX.Element;
-export {};
