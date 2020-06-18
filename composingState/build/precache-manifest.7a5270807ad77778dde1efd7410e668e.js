919d30defcbcf25a31aae065ecf76dec9addac27
almansa@conan-ui.com <almansa@conan-ui.com>
2020-06-18 16:34:14 +0200

    Uploading changes from source package.

---
+++ composingState/build/precache-manifest.7a5270807ad77778dde1efd7410e668e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28579c1151156aee0c5454500686e97f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f151635a2c8d0b5d41b",
+    "url": "/static/js/2.4264403a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4264403a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c4d813d9ac6715f27d",
+    "url": "/static/js/main.1c7b0796.chunk.js"
+  },
+  {
+    "revision": "7e5392bc926abec85bad",
+    "url": "/static/js/runtime-main.77b0b8f5.js"
+  }
+]);
--- composingState/build/service-worker.js
+++ composingState/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cea00abe1d714e6a47ed4e9d860eeb8.js"
+  "/precache-manifest.7a5270807ad77778dde1efd7410e668e.js"
 );
 
 self.addEventListener('message', (event) => {
