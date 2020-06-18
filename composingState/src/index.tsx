0c11a4e4a067df7612daa3b386738299592750b6
almansa@conan-ui.com <almansa@conan-ui.com>
2020-06-18 15:10:38 +0200

    Uploading changes from source package.

---
+++ composingState/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ComposeStateApp} from "./app";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ComposeStateApp/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
