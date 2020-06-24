9b99908fb214a1dcd3e594dcaabe80ef020e3fb5
almansa@conan-ui.com <almansa@conan-ui.com>
2020-06-24 18:22:14 +0200

    Uploading changes from source package.

--- todo-list-optimist/src/optimisticTodoDataState.ts
+++ todo-list-optimist/src/optimisticTodoDataState.ts
@@ -5,49 +5,35 @@ import {ToDo, VisibilityFilters} from "./domain/domain";
 import {MonitorStatus} from "conan-js-core";
 import {Lists} from "conan-js-core";
 
-export const OptimisticTodoListData$ = (todoListState: TodoListState): ConanState<OptimisticTodoListData, TodoListActions> => todoListState.asyncMerge<OptimisticTodoListData>(
-    {
-        appliedFilter: VisibilityFilters.SHOW_ALL,
-        todos: []
-    },
-    (monitorInfo, data, current) => {
-        if (monitorInfo.currentAction == null) {
-            return current;
-        }
+export function OptimisticTodoListData$ (todoListState: TodoListState): ConanState<OptimisticTodoListData, TodoListActions> {
+    return todoListState.asyncMerge<OptimisticTodoListData>(
+        {
+            appliedFilter: VisibilityFilters.SHOW_ALL,
+            todos: []
+        },
+        (monitorInfo, data, current) => {
+            if (monitorInfo.currentAction == null) {
+                return current;
+            }
 
-        let asyncTodo: ToDo = monitorInfo.currentAction.payload [0];
-        if (monitorInfo.status === MonitorStatus.ASYNC_CANCELLED) {
-            return ({
+            let asyncTodo: ToDo = monitorInfo.currentAction.payload [0];
+            if (monitorInfo.status === MonitorStatus.ASYNC_CANCELLED) {
+                return ({
+                    appliedFilter: current.appliedFilter,
+                    todos: current.todos.filter(it => it.data.id !== asyncTodo.id)
+                })
+            }
+
+            if (monitorInfo.status !== MonitorStatus.ASYNC_START) {
+                return current;
+
+            }
+
+            return monitorInfo.currentAction.name === 'addTodo' ? {
                 appliedFilter: current.appliedFilter,
-                todos: current.todos.filter(it => it.data.id !== asyncTodo.id)
-            })
-        }
-
-        if (monitorInfo.status !== MonitorStatus.ASYNC_START) {
-            return current;
-
-        }
-
-        return monitorInfo.currentAction.name === 'addTodo' ? {
-            appliedFilter: current.appliedFilter,
-            todos: [...current.todos, {
-                status: OptimisticStatus.IN_PROCESS,
-                data: asyncTodo,
-                cancelCb: () => {
-                    if (monitorInfo.currentAction == null) {
-                        throw new Error(`unexpected error`);
-                    }
-
-                    monitorInfo.currentAction.asap.cancel()
-                }
-            }]
-        } : monitorInfo.currentAction.name === 'toggleTodo' ? {
-                appliedFilter: current.appliedFilter,
-                todos: current.todos.map(todo => todo.data.id !== asyncTodo.id ? todo : {
+                todos: [...current.todos, {
                     status: OptimisticStatus.IN_PROCESS,
-                    data: {
-                        ...asyncTodo
-                    },
+                    data: asyncTodo,
                     cancelCb: () => {
                         if (monitorInfo.currentAction == null) {
                             throw new Error(`unexpected error`);
@@ -55,22 +41,38 @@ export const OptimisticTodoListData$ = (todoListState: TodoListState): ConanStat
 
                         monitorInfo.currentAction.asap.cancel()
                     }
+                }]
+            } : monitorInfo.currentAction.name === 'toggleTodo' ? {
+                    appliedFilter: current.appliedFilter,
+                    todos: current.todos.map(todo => todo.data.id !== asyncTodo.id ? todo : {
+                        status: OptimisticStatus.IN_PROCESS,
+                        data: {
+                            ...asyncTodo
+                        },
+                        cancelCb: () => {
+                            if (monitorInfo.currentAction == null) {
+                                throw new Error(`unexpected error`);
+                            }
+
+                            monitorInfo.currentAction.asap.cancel()
+                        }
+                    })
+                } :
+                current
+        },
+        (data, monitorInfo, optimisticData) => ({
+            appliedFilter: data.appliedFilter,
+            todos: Lists.mergeCombine(
+                data.todos,
+                optimisticData.todos,
+                (todo, optimisticTodo) => todo.id === optimisticTodo.data.id,
+                (todo, optimisticTod) => ({
+                    status: OptimisticStatus.SETTLED,
+                    data: todo,
+                    cancelCb: () => {
+                    }
                 })
-            } :
-            current
-    },
-    (data, monitorInfo, optimisticData) => ({
-        appliedFilter: data.appliedFilter,
-        todos: Lists.mergeCombine(
-            data.todos,
-            optimisticData.todos,
-            (todo, optimisticTodo) => todo.id === optimisticTodo.data.id,
-            (todo, optimisticTod) => ({
-                status: OptimisticStatus.SETTLED,
-                data: todo,
-                cancelCb: () => {
-                }
-            })
-        )
-    })
-)
+            )
+        })
+    )
+}
