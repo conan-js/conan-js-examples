36c34e6af41f013e1d67fbdb777ef76dff3ae972
almansa@conan-ui.com <almansa@conan-ui.com>
2020-07-07 18:19:48 +0200

    Uploading changes from source package.

---
+++ logging/build/precache-manifest.7cf3d984c3f115106af25e21d55c3c6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59c31a2c91ec90ff2d6110315ecaa46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992293908061f1287c3e",
+    "url": "/static/js/2.89e1ada3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89e1ada3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ae7777d30012a3ae34",
+    "url": "/static/js/main.1b2ef2c1.chunk.js"
+  },
+  {
+    "revision": "990495caa9c367cfe910",
+    "url": "/static/js/runtime-main.6ae43994.js"
+  }
+]);
