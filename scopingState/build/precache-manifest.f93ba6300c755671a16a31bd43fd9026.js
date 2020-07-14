6e742c6479458fbc2ffb3aa8f0e908d5a4e03832
almansa@conan-ui.com <almansa@conan-ui.com>
2020-07-14 19:44:50 +0200

    Uploading changes from source package.

---
+++ scopingState/build/precache-manifest.f93ba6300c755671a16a31bd43fd9026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfa16e9d2d99fa36deae4a86030dedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405844ad2d3338b0c8d4",
+    "url": "/static/js/2.b02b8205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b02b8205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd71f7ab254e0a49698",
+    "url": "/static/js/main.fb4f9835.chunk.js"
+  },
+  {
+    "revision": "de1bc5e1b435abea0e6d",
+    "url": "/static/js/runtime-main.1c84bb70.js"
+  }
+]);
---
+++ scopingState/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ScopingStateApp} from "./app";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ScopingStateApp/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
